44939b42c4fc7b62ecca8fd7abe25a8242efb030
MLpeek <m.leeb.it@gmail.com>
2021-05-10 17:49:49 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,13 @@ rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
 echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
 
+( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
+rabbitmqctl add_user $RABBITMQ_USER1 $RABBITMQ_PASSWORD1 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_USER1 administrator ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_USER1  ".*" ".*" ".*" ; \
+echo "*** User '$RABBITMQ_USER1' with password '$RABBITMQ_PASSWORD1' completed. ***" ; \
+echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
+
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
 # it will be as you run in the container rabbitmq-server arg1 arg2
